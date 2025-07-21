99a6e494d51c790fa9c86642965c19c2dc11a0cc
tjdwls5657@naver.com <tjdwls5657@naver.com>
2025-07-21 23:38:04 +0900

    커밋

--- client/mesh/line.js
+++ client/mesh/line.js
@@ -1,7 +1,15 @@
 import * as THREE from 'three';
 
 class LineMesh {
-    constructor(points){
-        const borderPoints = points.map(p => new THREE.Vectors(p.x, 0, p.y));
+    
+    constructor(points, option = {color: '#000', borderWidth: 1}){
+        const { color,  borderWidth} = option;
+        borderPoints.push(borderPoints[0]);
+        const borderMaterial = new THREE.LineBasicMaterial({
+            color,
+            linewidth: borderWidth
+        })
+
+        line = new THREE.LineLoop(borderGeometry, borderMaterial);
     }
 }
